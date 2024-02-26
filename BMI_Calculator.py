470e5cb16bc438cbe016a7bbbec4714d51946700
manojrenukumar <manojrenukumar8423@gmail.com>
2024-02-26 22:35:55 +1100

    BMI Initial code

---
+++ BMI_Calculator.py
@@ -0,0 +1,7 @@
+#input height
+height = float(input("Please enter your height in meters"))
+#input  weight
+weight = int(input("Please enter your weight in KG"))
+
+BMI = weight/ (height*height)
+print("Your BMI is:",BMI)
